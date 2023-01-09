b183a9d0549fd620ebb2fbf8134f783008e07b29
Riley Winkler <rex8112@gmail.com>
2023-01-09 16:51:42 -0600

    Add relational creation methods

--- scraper/database.py
+++ scraper/database.py
@@ -27,6 +27,25 @@ class User(BaseModel):
     id = BigIntegerField(unique=True, primary_key=True)
     private = BooleanField(default=False)
 
+    def add_sub_month(self, month: 'HumbleMonth') -> 'SubbedUserMonth':
+        conn = SubbedUserMonth(user=self, month=month)
+        conn.save()
+        return conn
+
+    def add_redeemed_game(self, game: 'HumbleGame', gift=False) -> 'RedeemedUserGame':
+        conn = RedeemedUserGame(user=self, game=game, gifted=gift)
+        conn.save()
+        return conn
+
+    def get_games(self):
+        query = (HumbleGame
+                 .select()
+                 .join(HumbleMonth)
+                 .join(SubbedUserMonth)
+                 .join(User)
+                 .where(User == self))
+        return query
+
 
 class SubbedUserMonth(BaseModel):
     user = ForeignKeyField(User)
@@ -35,6 +54,6 @@ class SubbedUserMonth(BaseModel):
 
 class RedeemedUserGame(BaseModel):
     user = ForeignKeyField(User)
-    month = ForeignKeyField(HumbleGame)
+    game = ForeignKeyField(HumbleGame)
     gifted = BooleanField(default=False)
 
